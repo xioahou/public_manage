ac56cfaf371220402f8fd4c0eaa122ec7627548e
Super <2747568334@qq.com>
2024-07-27 16:07:07 +0800

    登录样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import 'reset-css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '@/utils/element'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login_page">
+    <div class="login_box">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:'loginPage',
+data(){
+    return {
+
+    }
+}
+}
+</script>
+
+<style scoped lang="less">
+.login_page{
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('../../assets/loginbg.jpg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    .login_box{
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform: translate(-50%,-50%);
+        width: 400px;
+        height: 250px;
+        // background-color: #fff;
+        background: rgba(255, 255, 255, .5);
+	-webkit-backdrop-filter: blur(10px);
+	backdrop-filter: blur(5px);
+
+    }
+}
+</style>
